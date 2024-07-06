2ba884fbae1212b807c904cd71536c7185860a29
Shaikimram <shaikimran2723@gmail.com>
2024-07-06 13:18:44 +0000

    Initial comit - Add mi.c

---
+++ mi.c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int file_exists(const char *filename){
+return access(filename,F_OK) !=-1;
+}
+
+int move_file(const char *source,const char *destination){
+if(rename(source,destination)!=0){
+perror("Error moving file");
+return 1;
+}
+return 0;
+}
+
+int main(int argc,char *argv[]){
+if(argc !=3){
+fprintf(stderr,"usage: %s <source_file> <destination_file>\n",argv[0]);
+return 1;
+}
+const char *source =argv[1];
+const char *destination =argv[2];
+if(!file_exists(source)){
+fprintf(stderr,"usage: %s does not exists.\n",source);
+return 1;
+}
+if(move_file(source,destination) !=0){
+return 1;
+}
+printf("File moved successfully from %s to %s.\n",source,destination);
+return 0;
+
+
+
+
+
+}
